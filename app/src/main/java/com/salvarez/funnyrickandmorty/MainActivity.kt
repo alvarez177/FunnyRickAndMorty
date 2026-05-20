8d715ce15c2b980b940b6fda3c4a4a3effd7a40d
alvarez177 <alvarez177.sebastian@gmail.com>
2026-05-20 01:21:26 -0500

    First commit for link project with repository

---
+++ app/src/main/java/com/salvarez/funnyrickandmorty/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.salvarez.funnyrickandmorty
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import com.salvarez.funnyrickandmorty.ui.theme.FunnyRickAndMortyTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            FunnyRickAndMortyTheme { }
+        }
+    }
+}
