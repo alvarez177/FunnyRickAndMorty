71bdda5839e61ee9c642a6ce8b9bd07b6876aa2a
alvarez177 <alvarez177.sebastian@gmail.com>
2026-05-23 04:41:25 -0500

    Here I worked on #6 - Implementation for BaseViewModel.kt and GetRickAndMortyCharactersViewModel.kt

--- app/src/main/java/com/salvarez/funnyrickandmorty/presentation/viewmodel/BaseViewModel.kt
+++ app/src/main/java/com/salvarez/funnyrickandmorty/presentation/viewmodel/BaseViewModel.kt
@@ -1,4 +1,39 @@
 package com.salvarez.funnyrickandmorty.presentation.viewmodel
 
-class BaseViewModel {
+import androidx.lifecycle.ViewModel
+import com.salvarez.funnyrickandmorty.presentation.structuredefinition.Reducer
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.receiveAsFlow
+
+abstract class BaseViewModel<State : Reducer.ViewState, ViewIntent : Reducer.ViewIntent, Effect : Reducer.ViewEffect>(
+    private val initialState: State,
+    private val reducer: Reducer<State, ViewIntent, Effect>
+) : ViewModel() {
+    private val _state: MutableStateFlow<State> = MutableStateFlow(value = initialState)
+    val state: StateFlow<State> get() = _state.asStateFlow()
+
+    private val _event: MutableSharedFlow<ViewIntent> = MutableSharedFlow()
+    val event: SharedFlow<ViewIntent> get() = _event.asSharedFlow()
+
+    private val _effects: Channel<Effect> = Channel(capacity = Channel.Factory.CONFLATED)
+    val effects: Flow<Effect> = _effects.receiveAsFlow()
+
+    private fun sendEffect(effect: Effect) {
+        _effects.trySend(effect)
+    }
+
+    fun sendEvent(event: ViewIntent) {
+        val (newState, effect) = reducer.reduce(_state.value, event)
+        _state.tryEmit(newState)
+        effect?.let {
+            sendEffect(it)
+        }
+    }
 }
--- app/src/main/java/com/salvarez/funnyrickandmorty/presentation/viewmodel/GetRickAndMortyCharactersViewModel.kt
+++ app/src/main/java/com/salvarez/funnyrickandmorty/presentation/viewmodel/GetRickAndMortyCharactersViewModel.kt
@@ -1,4 +1,97 @@
 package com.salvarez.funnyrickandmorty.presentation.viewmodel
 
-class GetRickAndMortyCharactersViewModel {
+import androidx.lifecycle.viewModelScope
+import com.salvarez.domain.model.RickAndMortyError
+import com.salvarez.domain.model.RickAndMortyResult
+import com.salvarez.domain.usecase.GetRickAndMortyCharactersUseCase
+import com.salvarez.funnyrickandmorty.model.RickAndMortyCharacterSupportingVisual
+import com.salvarez.funnyrickandmorty.presentation.structuredefinition.RickAndMortyEffect
+import com.salvarez.funnyrickandmorty.presentation.structuredefinition.RickAndMortyIntent
+import com.salvarez.funnyrickandmorty.presentation.structuredefinition.RickAndMortyReducer
+import com.salvarez.funnyrickandmorty.presentation.structuredefinition.RickAndMortyScreenState
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+class GetRickAndMortyCharactersViewModel @Inject constructor(
+    private val getRickAndMortyCharacters: GetRickAndMortyCharactersUseCase
+) :
+    BaseViewModel<RickAndMortyScreenState, RickAndMortyIntent, RickAndMortyEffect>(
+        initialState = RickAndMortyScreenState(),
+        reducer = RickAndMortyReducer()
+    ) {
+
+    init {
+        fetchRickAndMortyCharacters()
+    }
+
+    fun fetchRickAndMortyCharacters() {
+        viewModelScope.launch {
+            sendEvent(RickAndMortyIntent.ShowLoading)
+            val rickAndMortyCharactersResult = getRickAndMortyCharacters.invoke()
+            when (rickAndMortyCharactersResult) {
+                is RickAndMortyResult.Error -> {
+                    when (rickAndMortyCharactersResult) {
+                        RickAndMortyError.NoInternet,
+                        RickAndMortyError.Timeout -> {
+                            sendEvent(
+                                event = RickAndMortyIntent.ShowError(
+                                    RickAndMortyError.NoInternet,
+                                    title = "Error de conexión a internet ó expiro el tiempo de la solicitud",
+                                    subtitle = "Intentalo mas tarde."
+                                )
+                            )
+                        }
+
+                        RickAndMortyError.Server,
+                        RickAndMortyError.ServiceUnavailable -> {
+                            sendEvent(
+                                event = RickAndMortyIntent.ShowError(
+                                    RickAndMortyError.Server,
+                                    title = "Error de conexión con el servidor",
+                                    subtitle = "Intentalo mas tarde."
+                                )
+                            )
+                        }
+
+                        RickAndMortyError.EmptyCharacters -> {
+                            sendEvent(
+                                event = RickAndMortyIntent.ShowError(
+                                    RickAndMortyError.EmptyCharacters,
+                                    title = "No se encontrarón caracteres",
+                                    subtitle = "Intentalo mas tarde."
+                                )
+                            )
+                        }
+
+                        RickAndMortyError.Unknown -> {
+                            sendEvent(
+                                event = RickAndMortyIntent.ShowError(
+                                    RickAndMortyError.Unknown,
+                                    title = "Error desconocido",
+                                    subtitle = "Contacte a soporte."
+                                )
+                            )
+                        }
+                    }
+                }
+
+                is RickAndMortyResult.Success -> {
+                    val rickAndMortyCharacters = rickAndMortyCharactersResult.data
+                    sendEvent(
+                        RickAndMortyIntent.ShowRickAndMortyCharacters(
+                            rickAndMortyCharacters = rickAndMortyCharacters.map { rickAndMortyCharacter ->
+                                RickAndMortyCharacterSupportingVisual(
+                                    id = rickAndMortyCharacter.id,
+                                    name = rickAndMortyCharacter.name,
+                                    status = rickAndMortyCharacter.status,
+                                    species = rickAndMortyCharacter.species,
+                                    gender = rickAndMortyCharacter.gender,
+                                    image = rickAndMortyCharacter.image,
+                                )
+                            }
+                        ))
+                }
+            }
+        }
+    }
 }
